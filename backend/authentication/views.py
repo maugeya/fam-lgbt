f23b6ed00fa939b1b709a4278c0325ee7464e054
Angela Maugey <a.maugey@gmail.com>
2021-03-20 20:24:28 +0000

    Creating register user view, login view and a WIP protested EditProfileView

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+from .serializers import TokenObtainPairSerializer, CustomUserSerializer
+
+
+class ObtainTokenPairView(TokenObtainPairView):
+    """Login view"""
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = TokenObtainPairSerializer
+
+class CustomUserCreate(APIView):
+    """
+    Register new user view
+    """
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format="json"):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EditProfileView(APIView):
+    """
+    A protected view for editing user profiles
+    """
+
+    def get(self, request):
+        return Response (data={"hello": "world"}, status=status.HTTP_200_OK)
