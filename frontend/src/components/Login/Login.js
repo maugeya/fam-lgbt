83502b9ebdbe07bab4158040d6d5b8a8c56a64e2
Angela Maugey <a.maugey@gmail.com>
2021-03-26 14:03:34 +0000

    Basic Login component

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,7 +1,67 @@
-import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Login = () => {
-  return <div>LOGIN ROUTE</div>;
+  const [inputValues, setInputValues] = useState({
+    username: '',
+    password: '',
+  });
+  const [errors, setErrors] = useState([]);
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('/api/auth/token/obtain/', {
+        username: inputValues.username,
+        password: inputValues.password,
+      });
+      console.log('res', res);
+      // TODO: Send to Authenticated Home
+    } catch (err) {
+      setErrors([...errors, err.response.data]);
+    }
+  };
+
+  const handleOnChangeInput = (e) => {
+    const { name, value } = e.target;
+    setInputValues({
+      ...inputValues,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div>
+      Login
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label>
+            Username
+            <input
+              type='text'
+              placeholder='username'
+              name='username'
+              onChange={handleOnChangeInput}
+              value={inputValues.username}
+            />
+          </label>
+          <label>
+            Password
+            <input
+              type='password'
+              placeholder='password'
+              name='password'
+              onChange={handleOnChangeInput}
+              value={inputValues.password}
+            />
+          </label>
+        </div>
+        <div>
+          <button type='submit'>Log in</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
