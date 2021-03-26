94915b7c6cccc49d8b723b0079908232f5c7d686
Angela Maugey <a.maugey@gmail.com>
2021-03-26 14:03:46 +0000

    Basic register component

--- frontend/src/components/Register/Register.js
+++ frontend/src/components/Register/Register.js
@@ -1,7 +1,79 @@
-import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
 
 const Register = () => {
-  return <div>Register route</div>;
+  const [inputValues, setInputValues] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const [errors, setErrors] = useState([]);
+
+  const handleOnChangeInput = (e) => {
+    const { name, value } = e.target;
+
+    setInputValues({
+      ...inputValues,
+      [name]: value,
+    });
+  };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('/api/auth/user/create/', {
+        username: inputValues.username,
+        email: inputValues.email,
+        password: inputValues.password,
+      });
+      // TODO: send to Login page
+    } catch (err) {
+      setErrors([...errors, err.response.data]);
+    }
+  };
+
+  return (
+    <div>
+      Register
+      <form onSubmit={handleOnSubmit}>
+        <div></div>
+        <label>
+          Username
+          <input
+            type='text'
+            placeholder='username'
+            name='username'
+            onChange={handleOnChangeInput}
+            value={inputValues.username}
+          />
+        </label>
+        <label>
+          Email
+          <input
+            type='email'
+            placeholder='email'
+            name='email'
+            onChange={handleOnChangeInput}
+            value={inputValues.email}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type='password'
+            placeholder='password'
+            name='password'
+            onChange={handleOnChangeInput}
+            value={inputValues.password}
+          />
+        </label>
+        <div>
+          <button type='submit'>Register</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
