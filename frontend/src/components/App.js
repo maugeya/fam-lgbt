8c36c66c90a1aed793ef0e7e2fcf70aba0fa7fb1
Angela Maugey <a.maugey@gmail.com>
2021-03-28 21:26:04 +0100

    Random App file change

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import Hello from './Hello/Hello';
