88bca8e4ec2392d720a115caf1352aee703bf789
Angela Maugey <a.maugey@gmail.com>
2021-03-28 20:03:39 +0100

    Adding logout view

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -12,6 +13,25 @@ class ObtainTokenPairView(TokenObtainPairView):
     """Login view"""
     permission_classes = (permissions.AllowAny,)
     serializer_class = TokenObtainPairSerializer
+
+
+class LogoutView(APIView):
+    """
+    Logout view.
+    Will also Blacklist Refresh Token
+    """
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = ()
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
 
 class CustomUserCreate(APIView):
     """
