6873e29cb364f68480448651c897afa127995c47
Angela Maugey <a.maugey@gmail.com>
2021-03-28 20:03:48 +0100

    Adding logout url

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
-from .views import ObtainTokenPairView, CustomUserCreate, EditProfileView
+from .views import ObtainTokenPairView, CustomUserCreate, EditProfileView, LogoutView
 
 urlpatterns = [
     path("user/create/", CustomUserCreate.as_view(), name="create_user"),
     path("token/obtain/", ObtainTokenPairView.as_view(), name="token_create"),
     path("token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("hello/", EditProfileView.as_view(), name="edit_profile")
 ]
