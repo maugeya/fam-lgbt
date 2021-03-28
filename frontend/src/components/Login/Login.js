2827be324becbacd01a8ed1f0f35dd2d76d720ab
Angela Maugey <a.maugey@gmail.com>
2021-03-28 20:35:58 +0100

    Updating Login component

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -14,21 +14,16 @@ const Login = () => {
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axiosInstance.post(
-        '/auth/token/obtain/',
-        {
-          username: inputValues.username,
-          password: inputValues.password,
-        }
-        // { withCredentials: true }
-      );
+      const res = await axiosInstance.post('/auth/token/obtain/', {
+        username: inputValues.username,
+        password: inputValues.password,
+      });
       axiosInstance.defaults.headers['Authorization'] =
         'JWT ' + res.data.access;
       localStorage.setItem('access_token', res.data.access);
       localStorage.setItem('refresh_token', res.data.refresh);
       history.push('/hello');
       return res.data;
-      // TODO: Send to Authenticated Home
     } catch (err) {
       setErrors([err.response.data]);
     }
