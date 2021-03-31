2e6c434d7f5afd28deeef4de207f1a5f9874ae65
Angela Maugey <a.maugey@gmail.com>
2021-03-31 22:19:00 +0100

    Adding userReducer to rootReducer

--- frontend/src/redux/rootReducer.js
+++ frontend/src/redux/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import counterReducer from './Counter/counter.reducer';
+import { userReducer } from './User/user.reducers';
 
 const rootReducer = combineReducers({
   counter: counterReducer,
+  currentUser: userReducer,
 });
 
 export default rootReducer;
