113a3debf1190bd1a8797c09e3995dfa3e1fe8b7
Angela Maugey <a.maugey@gmail.com>
2021-04-04 21:51:49 +0100

    Updating reducer returns

--- frontend/src/redux/Alert/alert.reducers.js
+++ frontend/src/redux/Alert/alert.reducers.js
@@ -6,13 +6,15 @@ export const alertReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case ALERT_SUCCESS:
       return {
-        type: 'alert-success',
+        type: 'Success',
         message: action.message,
+        backgroundColor: '#5cb85c',
       };
     case ALERT_FAIL:
       return {
-        type: 'alert-fail',
+        type: 'Fail',
         message: action.message,
+        backgroundColor: '#d9534f',
       };
     case ALERT_CLEAR:
       return INITIAL_STATE;
