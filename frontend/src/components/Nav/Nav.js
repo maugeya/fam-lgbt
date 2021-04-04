b9579557443d65a5e2fbc8e19956c13dc4514cb2
Angela Maugey <a.maugey@gmail.com>
2021-04-04 10:38:05 +0100

    Using new service in handleClick event on pushing logout button

--- frontend/src/components/Nav/Nav.js
+++ frontend/src/components/Nav/Nav.js
@@ -2,32 +2,14 @@ import { Link, useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
 import styles from './Nav.module.css';
-import axiosInstance from '../../axiosApi';
-import {
-  logoutRequest,
-  logoutSuccess,
-  logoutFail,
-} from '../../redux/User/user.actions';
+
+import { userLogoutService } from '../../redux/User/user.services';
 
 const Nav = () => {
   const dispatch = useDispatch();
   let history = useHistory();
-  const handleLogout = async () => {
-    try {
-      dispatch(logoutRequest());
-      const res = await axiosInstance.post('/auth/logout/', {
-        refresh_token: localStorage.getItem('refresh_token'),
-      });
-      localStorage.removeItem('access_token');
-      localStorage.removeItem('refresh_token');
-      axiosInstance.defaults.headers['Authorization'] = null;
-      dispatch(logoutSuccess());
-      history.push('/');
-      return res;
-    } catch (e) {
-      dispatch(logoutFail());
-      console.log(e);
-    }
+  const handleLogout = () => {
+    userLogoutService(dispatch, history);
   };
 
   return (
