e792b137b59d52f1bef018f5277a3d87bba9c3d3
Angela Maugey <a.maugey@gmail.com>
2021-05-14 13:43:49 +0100

    remove counterReducer from rootReducer

--- frontend/src/redux/rootReducer.js
+++ frontend/src/redux/rootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 
 import { alertReducer } from './Alert/alert.reducers';
-import counterReducer from './Counter/counter.reducer';
 import { userReducer } from './User/user.reducers';
 
 const rootReducer = combineReducers({
-  counter: counterReducer,
   currentUser: userReducer,
   alertMessages: alertReducer,
 });
