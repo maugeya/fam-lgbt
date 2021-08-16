73741826263ea50f06e464ca39b978b0a79f16e9
Angela Maugey <a.maugey@gmail.com>
2021-08-16 22:06:54 +0100

    Add user app. Create Profile model, hook up to signals to create on CustomUser save

---
+++ backend/user/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from authentication.models import CustomUser
+
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+
+
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=CustomUser)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
